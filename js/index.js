f52aae911f3fb1a51057f35bac2ed939d86c0372
Михаил Виноградчий <saltiforvdeveloper@gmail.com>
2021-04-24 11:33:54 +0300

    git init

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((t / 1000) % 60);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    return {
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    const minutesSpan = document.querySelector('.minutes');
+    const secondsSpan = document.querySelector('.seconds');
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+}
+let now = moment()
+const deadline = now.add(5,'minutes')
+initializeClock('countdown', deadline);
